c28419ec42da400f9d81be1a10cd27df9b9dd02b
Alex Schmoe <aschmoe@gmail.com>
2016-10-05 13:10:43 -0700

    adding hover effects to result listings

--- docroot/public/js/controllers/resource/resourceCtrl.js
+++ docroot/public/js/controllers/resource/resourceCtrl.js
@@ -41,7 +41,31 @@ angular.module('app')
   }
 
   // Get help text
-  $scope.getTypeHelp = function(item) {
+  $scope.getStarHelp = function(item) {
+    $text = '';
+    switch(item.QualityRating) {
+      case '1':
+        $text = "A star rating is received through an evaluation process by the state.  One star signifies that the facility has a current MS child care license and has registered for the Quality Star program.";
+        break;
+      case '2':
+        $text = "A star rating is received through an evaluation process by the state.   Two stars signifies that: the center meets all the qualifications of a 1 star facility and has a administration policy handbook; director and teaching staff receive and document 20 and 15 hours of professional development by certified trainers annually; teachers use weekly lesson plans and learning centers (excluding infant room), and have received a minimum score of 3.00 on the Early Childhood or Infant/Toddler Environment Rating scale; the center has a bulletin board, quarterly newsletter and monthly calendar of activities and a documented annual parent-teacher conference; and the director completes and files a self-assessment with an improvement plan, and an annual signed staff evaluation.";
+        break;
+      case '3':
+        $text = "A star rating is received through an evaluation process by the state.   Three stars signifies that: the center meets all the qualifications of a 2 star facility and the director has completed a Child Care as a business training and has an MOU with the service agency for themselves and all staff; Director holds a qualifying credential or childhood education-related degree, conducts monthly staff development meetings, one staff has a qualifying credential or childhood education-related degree, 18 hours of annual training for staff, plus an additional 10 hours annually of specialized age group training; Director is trained in MS Early Learning Guidelines; have received a minimum score of 3.60 on the Early Childhood or Infant/Toddler Environment Rating scale; weekly notes of activities distributed to parents, offers parent education and training activities annually; provides a parent/family lending library.";
+        break;
+      case '4':
+        $text = "A star rating is received through an evaluation process by the state.   Four stars signifies that: the center meets all the qualifications of a 3 star facility and documentation and implementation of a professional development plan for staff; Director holds an associate’s degree or higher in Early Childhood Education or Childhood Development Technology; 15% of staff have a child development associate credential or higher degree; 20 hours of annual staff development training, plus an additional 10 hours of specialized age group training; all teaching staff trained to teach the MS Early Learning Guidelines for the age group they teach;  have received a minimum score of 4.10 on the Early Childhood or Infant/Toddler Environment Rating scale; parent/family volunteer program is implemented and documented; parent/family resource center is part of facility’s services; parent/family survey to rate performance of staff on file.";
+        break;
+      case '5':
+        $text = "A star rating is received through an evaluation process by the state.   Five stars signifies that: the center meets all the qualifications of a 4 star facility and developmental checklist for each child is implemented and documented; there is a transition plan with local education agency for kindergarten; Director to peer mentoring on quality star program; Director holds bachelor’s degree or higher in Early Childhood Education, Early Childhood Special Education, Childhood Development, Elementary Education or related field; 25 % of full-time teachers have Child Development Associate credential or higher; 25 hours of annual training plus 10 hours in specialized age group; MS Early Learning Guidelines fully implemented in all classrooms; ongoing child assessments implemented and documented; have received a minimum score of 5.10 on the Early Childhood or Infant/Toddler Environment Rating scale; documentation showing that parent/teacher conferences conducted bi-annually; monthly newsletters to parents.";
+        break;
+  
+    }
+    return $text;
+  }
+
+  // Get help text
+  $scope.getProviderTypeHelp = function(item) {
     $text = '';
     switch(item.ProviderTypeDescription) {
       case 'Center':
@@ -53,11 +77,20 @@ angular.module('app')
               + "meaning the center must adhere to state childcare regulations and ensure health and safety priorities.  Centers may voluntarily participate in MS' Quality Star program.  A higher star rating suggests higher quality program, which may include offering educational or development curriculum by trained staff.";
         break;
 
-      case 'Non-relative in home':
+      case 'Non-Relative In-Home':
         $text = "An individual who provides child care services in the child's own home but maintains a separate residence."
         break;
 
-      case 'Non-relative out of home':
+      case 'Non-Relative Out-of-Home':
+        $text = "A setting in which an individual is paid by parents for care for their children while they are working or unavailable.  This setting occurs in provider's home, providing small group care in a home-like environment for fewer than six children.  These homes are NOT licensed, monitored, or otherwise regulated by the Mississippi Department of Health. Minimal information is maintained about these programs by the Mississippi Department of Health to assist in emergency preparedness only. Registered Family Daycare Homes may choose to participate in the same quality evaluation program offered to Licensed Family Daycare Homes.";
+
+        break;
+
+      case 'Relative In-Home':
+        $text = "An individual who provides child care services in the child's own home but maintains a separate residence."
+        break;
+
+      case 'Relative Out-of-Home':
         $text = "A setting in which an individual is paid by parents for care for their children while they are working or unavailable.  This setting occurs in provider's home, providing small group care in a home-like environment for fewer than six children.  These homes are NOT licensed, monitored, or otherwise regulated by the Mississippi Department of Health. Minimal information is maintained about these programs by the Mississippi Department of Health to assist in emergency preparedness only. Registered Family Daycare Homes may choose to participate in the same quality evaluation program offered to Licensed Family Daycare Homes.";
 
         break;
@@ -67,7 +100,6 @@ angular.module('app')
         break;
   
     }
-
     return $text;
   }
 
